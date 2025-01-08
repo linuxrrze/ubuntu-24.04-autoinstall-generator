fa9ccc665ab452634e281db9930ff0ad0181e24d
Marcel Ritter <marcel+git@linux-ng.de>
2025-01-08 20:57:20 +0100

    Switch to pushd/popd

--- ubuntu-autoinstall-generator.sh
+++ ubuntu-autoinstall-generator.sh
@@ -272,9 +272,9 @@ else
 fi
 
 log "📦 Repackaging extracted files into an ISO image..."
-cd "$tmpdir/iso"
+pushd "$tmpdir/iso"
 xorriso -as mkisofs -r -V "ubuntu-autoinstall-$today" -o "${destination_iso}" --grub2-mbr ../BOOT/1-Boot-NoEmul.img -partition_offset 16 --mbr-force-bootable -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b ../BOOT/2-Boot-NoEmul.img -appended_part_as_gpt -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 -c '/boot.catalog' -b '/boot/grub/i386-pc/eltorito.img' -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info -eltorito-alt-boot -e '--interval:appended_partition_2:::' -no-emul-boot .
-cd "$OLDPWD"
+popd
 log "👍 Repackaged into ${destination_iso}"
 
 die "✅ Completed." 0
